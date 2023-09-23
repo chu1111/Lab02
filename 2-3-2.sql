deb3f13fdd1ee33d7db682abb8cd48ae5b1170e8
chu1111 <113088657+chu1111@users.noreply.github.com>
2023-09-23 11:18:39 +0800

    Add files via upload

---
+++ 2-3-2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Department
+(
+Dep_id CHAR(4),
+Dep_name nvarchar(10) not null,
+Dep_head nvarchar(10) not null,
+primary key(Dep_id)
+)
+
---
+++ 2-3-ex01.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Student
+(
+stu_id CHAR(5),
+stu_name nvarchar(10) not null,
+dep_code nvarchar(4) not null,
+primary key(stu_id)
+)
